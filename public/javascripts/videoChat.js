451ab01e94bd33ca47341a501d9a0ba943ce019e
Muhammad Umer <techsin@Muhammads-Air.home>
2017-10-28 22:47:58 -0400

    work

--- public/javascripts/videoChat.js
+++ public/javascripts/videoChat.js
@@ -40,11 +40,12 @@ async function setupWebRTC(initiator) {
         urls: "stun:stun1.l.google.com:19302"
       }, {
         urls: "stun:stun2.l.google.com:19302"
-      }, {
-        urls: "stun:stun3.l.google.com:19302"
-      }, {
-        urls: "stun:stun4.l.google.com:19302"
-      },
+      }, 
+      // {
+      //   urls: "stun:stun3.l.google.com:19302"
+      // }, {
+      //   urls: "stun:stun4.l.google.com:19302"
+      // },
       {
         urls: "turn:numb.viagenie.ca",
         username: "myemailum14@gmail.com",
